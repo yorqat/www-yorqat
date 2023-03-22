7582256ba5436f7bc017e6c18da0c80f11b3da17
Qark-dev <qarkdev@gmail.com>
2023-03-22 12:04:14 +0800

    define layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Heading from "./Heading.svelte";
+</script>
+
+<Heading />
+<slot />
+
+<style lang="scss">
+    :global(:root) {
+        --clr-text: aliceblue;
+        --clr-text-reading: rgb(187, 189, 229);
+        --clr-primary: rgb(17, 17, 20);
+        --clr-secondary: rgb(10, 9, 11);
+        --clr-accent-0: rgb(209, 129, 209);
+    }
+</style>
