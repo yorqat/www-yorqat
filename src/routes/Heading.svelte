fb6e7c40ca5277b396c1ecdcbb2331c894855986
Qark-dev <qarkdev@gmail.com>
2023-03-23 16:37:35 +0800

    change font

--- src/routes/Heading.svelte
+++ src/routes/Heading.svelte
@@ -1,6 +1,6 @@
 <div class="home-header">
     <a class="home-header-title" href="/">
-        <span>Yor Qat</span>
+        <span>Y<span class="smol">or</span>Q<span class="smol">at</span></span>
     </a>
     <a href="https://github.com/YorQat">
         <span><i class="fa-brands fa-github"></i></span>
@@ -8,10 +8,14 @@
 </div>
 
 <style lang="scss">
-	@import url('https://fonts.googleapis.com/css2?family=Righteous&display=swap');
+	@import url('https://fonts.googleapis.com/css2?family=Autour+One&display=swap');
 
     a {
         color: var(--clr-text);
+    }
+
+    .smol {
+        font-size: smaller;
     }
 
     .home-header {
@@ -22,7 +26,8 @@
         padding: 1rem;
 
         font-size: x-large;
-        font-family: 'Righteous', cursive;
+        font-weight: 600;
+        font-family: 'Autour One', cursive;
 
         display: flex;
         justify-content: space-between;
