2008a16aee47e6e67304b7e7b7dbae8842c58a34
YorQat <qarkdev+gh@gmail.com>
2023-05-13 16:28:35 +0800

    new colors

--- src/routes/(nav)/+layout.svelte
+++ src/routes/(nav)/+layout.svelte
@@ -12,6 +12,8 @@
         --clr-primary: rgb(25, 23, 32);
         --clr-secondary: rgb(10, 9, 11);
         --clr-accent-0: rgb(209, 129, 209);
-        --fltr-link: invert(77%) sepia(66%) saturate(5130%) hue-rotate(224deg) brightness(90%) contrast(91%);
+		--fltr-link: invert(77%) sepia(66%) saturate(5130%) hue-rotate(224deg) brightness(90%)
+			contrast(91%);
+		--fltr-white: brightness(0) invert(1);
     }
 </style>
