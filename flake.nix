0d31e49af6b14fbd69f557d8538833dcb8ea3df8
YorQat <qarkdev+gh@gmail.com>
2023-05-13 16:13:49 +0800

    use nix flakes for convenience

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  # Use the unstable nixpkgs to use the latest set of node packages
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          # pkgs.nodejs
+          # You can set the major version of Node.js to a specific one
+          pkgs.nodejs-19_x
+
+          pkgs.nodePackages.pnpm
+          # pkgs.yarn
+
+          pkgs.nodePackages.typescript
+          pkgs.nodePackages.typescript-language-server
+        ];
+      };
+    });
+}
