1afb152cc7c4ee218c84a5f7f45457ff19547a80
YorQat <qarkdev+gh@gmail.com>
2023-07-24 19:10:31 +0800

    update links

--- src/routes/(nav)/css-snips/+page.svelte
+++ src/routes/(nav)/css-snips/+page.svelte
@@ -8,11 +8,14 @@
 
 <section id="backstage">
 	<div class="snips">
-		<a href="snips/title-0">
-			<iframe src="snips/title-0" title="" />
+		<a href="/css-snips/login">
+			<iframe src="/css-snips/login" title="" />
 		</a>
-		<a href="snips/title-1">
-			<iframe src="snips/title-1" title="" />
+		<a href="/css-snips/fancy-menu">
+			<iframe src="/css-snips/fancy-menu" title="" />
+		</a>
+		<a href="/css-snips/mouse-chaser">
+			<iframe src="/css-snips/mouse-chaser" title="" />
 		</a>
 	</div>
 </section>
@@ -33,6 +36,18 @@
 		pointer-events: none;
 		width: 100%;
 		aspect-ratio: 16 /9;
+
+
+		--zoom: 0.6;
+
+		zoom: var(--zoom);
+		-ms-zoom: var(--zoom);
+        -moz-transform: scale(var(--zoom));
+        -moz-transform-origin: 0 0;
+        -o-transform: scale(var(--zoom));
+        -o-transform-origin: 0 0;
+        -webkit-transform: scale(var(--zoom));
+        -webkit-transform-origin: 0 0;
 	}
 
 	.snips {
