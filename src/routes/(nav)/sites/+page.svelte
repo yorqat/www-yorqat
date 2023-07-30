5d2a2253c957e2cb92fd9fb0aaf783f0271f9363
YorQat <qarkdev+gh@gmail.com>
2023-07-30 18:30:30 +0800

    add image

--- src/routes/(nav)/sites/+page.svelte
+++ src/routes/(nav)/sites/+page.svelte
@@ -1,3 +1,14 @@
+<script>
+	import hacker3 from "$lib/images/hacker3.jpg";
+</script>
+
 <section id="mockups">
 	<h1>Always determined for results</h1>
+	<img class="hacker-img" src={hacker3} alt="hacker" />
 </section>
+
+<style>
+	.hacker-img {
+		max-width: 140%;
+	}
+</style>
