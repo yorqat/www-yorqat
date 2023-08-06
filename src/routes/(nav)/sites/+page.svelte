76d6d6b0dba4e7911c58f72cdc0e7689a2a69992
YorQat <qarkdev+gh@gmail.com>
2023-08-06 15:42:56 +0800

    re-rolling sites

--- src/routes/(nav)/sites/+page.svelte
+++ src/routes/(nav)/sites/+page.svelte
@@ -2,13 +2,22 @@
 	import hacker3 from "$lib/images/hacker3.jpg";
 </script>
 
-<section id="mockups">
-	<h1>Always determined for results</h1>
+<section id="sites">
+	<h1 class="pretext">It's fun making websites</h1>
+	<p class="pretext subtitle">They're so webby and insightful. I'm almost sure of it.</p>
 	<img class="hacker-img" src={hacker3} alt="hacker" />
 </section>
 
 <style>
 	.hacker-img {
 		max-width: 140%;
+	}
+
+	.subtitle {
+		color: var(--clr-subtitle);
+	}
+
+	.pretext {
+		padding-inline: 2rem;
 	}
 </style>
